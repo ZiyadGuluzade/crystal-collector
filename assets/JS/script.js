e9747235e2181d2b80b9020197817c4f92a736a8
Z.Guluzade <ziyad_guluzade@outlook.com>
2019-02-11 20:13:51 -0500

    updated final version

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,8 +1,25 @@
 //variables
-var blueCrystal = 0;
-var yellowCrystal = 0;
-var greenCrystal = 0;
-var redCrystal = 0;
+var crystals = {
+    green: {
+        name: "Green",
+        value: 0
+        },
+
+    red: {
+        name: "Red",
+        value: 0
+        },
+
+    yellow: {
+        name: "Yellow",
+        value: 0
+        },
+
+    blue: {
+        name: "Blue",
+        value: 0
+        }
+    }
 
 var userScore = 0;
 var targetScore = 0;
@@ -11,58 +28,69 @@ var wins = 0;
 var losses = 0;
 
 
-// functions
-function startGame() {
+// restart function
+function restart() {
   // reset user score
   userScore = 0;
-  // set new target score between 19 - 120
+  //new target score between 19 - 120
   targetScore = randomNumber(19, 120);
-  // set new random value for each crystal between 1 - 12
-  blueCrystal = randomNumber(1, 12);
-  yellowCrystal = randomNumber(1, 12);
-  greenCrystal = randomNumber(1, 12);
-  redCrystal = randomNumber(1, 12);
+  //new random value for each crystal between 1 - 12
+  crystals.yellow.value = randomNumber(1, 12);
+  crystals.green.value = randomNumber(1, 12);
+  crystals.red.value = randomNumber(1, 12);
+  crystals.blue.value = randomNumber(1, 12);
 
   $("#userScore").html(userScore);
   $("#targetScore").html(targetScore);
-
-  console.log("------------------------");
-  console.log("The Target score is " + targetScore);
-  console.log("------------------------");
-  console.log("The blue crystal is " + blueCrystal);
-  console.log("The yellow crystal is " + yellowCrystal);
-  console.log("The Red crystal is " + redCrystal);
-  console.log("The green crystal is "+ greenCrystal);
-  console.log("------------------------");
-
-  var blueValue = function() {
-      
-  }
 }
 
+
+var crystalValues = function(crystals) {
+    userScore = userScore + crystals.value;
+    checkPoint();
+}
+
+//creating a function to check if the user value larger than target value
+function checkPoint() {
+    if (userScore > targetScore) {
+        losses ++;
+        // if user value is larger than target value then game add +1 to losses and restarts
+        $("#lost").html(losses);  
+        restart();
+    } else if (targetScore === userScore) {
+        wins ++; 
+        // if user value is equal to target value then game add +1 to wins and restarts
+        $("#won").html(wins);
+        restart();
+    } 
+    
+}
+
+// creating a function than generates a random number by taking two arguments
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
-
-//main section
+// creating onclick functions for every crystal that add value of the crystal to the user score
 $("#blue").click(function() {
-    function blueValue() {
-        userScore = userScore + blueCrystal;
-        console.log("yout score: " + userScore);
-    }
+    crystalValues(crystals.blue);
+    $("#userScore").html(userScore);
 });
 
 $("#green").click(function() {
-    alert("green");
+    crystalValues(crystals.green);
+    $("#userScore").html(userScore);
 });
 
 $("#red").click(function() {
-    alert("red");
+    crystalValues(crystals.red);
+    $("#userScore").html(userScore);
 });
 
 $("#yellow").click(function() {
-    alert("yellow");
+    crystalValues(crystals.yellow);
+    $("#userScore").html(userScore);
 });
 
-startGame();
+
+restart();
